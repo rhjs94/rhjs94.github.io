e5818c5521a256a192b1f3ceee4dcb6d60c5ab46
Richard Schmoetten <rhjs94@gmail.com>
2019-01-09 22:01:14 +0000

    board redrawn

--- onitama/onitama.js
+++ onitama/onitama.js
@@ -1,6 +1,11 @@
 function drawBoard(){
   var c = document.getElementById("boardCanvas");
   var ctx = c.getContext("2d");
+  ctx.moveTo(0,0);
+  ctx.lineTo(502,0);
+  ctx.lineTo(502,502);
+  ctx.lineTo(0,502);
+  ctx.lineTo(0,0);
   ctx.moveTo(101,0);
   ctx.lineTo(101,502);
   ctx.moveTo(201,0);
