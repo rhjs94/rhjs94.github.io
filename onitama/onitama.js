cf92928c62ca6c668a35651b26823a50360dfa35
Richard Schmoetten <rhjs94@gmail.com>
2019-01-10 22:07:45 +0000

    separate files for functions and classes. mouse tracking, but not tested.

--- onitama/onitama.js
+++ onitama/onitama.js
@@ -15,34 +15,17 @@ ctx.strokeStyle = "lightgray";
 ctx.lineWidth = 2;
 */
 
-// listen for mousedown events
-$("#canvas").mousedown(function (e) {
-    handleMouseDown(e);
-});
+drawBoard();
 
-function drawBoard(){
-  var c = document.getElementById("boardCanvas");
-  var ctx = c.getContext("2d");
-  ctx.moveTo(0,0);
-  ctx.lineTo(502,0);
-  ctx.lineTo(502,502);
-  ctx.lineTo(0,502);
-  ctx.lineTo(0,0);
-  ctx.moveTo(101,0);
-  ctx.lineTo(101,502);
-  ctx.moveTo(201,0);
-  ctx.lineTo(201,502);
-  ctx.moveTo(301,0);
-  ctx.lineTo(301,502);
-  ctx.moveTo(401,0);
-  ctx.lineTo(401,502);
-  ctx.moveTo(0,101);
-  ctx.lineTo(502,101);
-  ctx.moveTo(0,201);
-  ctx.lineTo(502,201);
-  ctx.moveTo(0,301);
-  ctx.lineTo(502,301);
-  ctx.moveTo(0,401);
-  ctx.lineTo(502,401);
-  ctx.stroke();
+// make shapes
+var boardFields = [];
+for (var i=0; i<5; i++) {
+  for (var j=0; j<5; j++) {
+    boardFields.push( new Square( { x:1+i*100, y:1+j*100}, 100 ) );
+  }
 }
+
+// listen for mousedown events
+$("#boardCanvas").mousedown(function (e) {
+    handleMouseDown(e, boardFields, ctxBoard, offsetXBoard, offsetYBoard);
+});
