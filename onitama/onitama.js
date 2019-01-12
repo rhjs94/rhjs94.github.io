1696279bd847b09b83dee27bec2ce63c38b0a175
Richard Schmoetten <rhjs94@gmail.com>
2019-01-12 11:38:29 +0000

    drawPieces added.

--- onitama/onitama.js
+++ onitama/onitama.js
@@ -15,31 +15,6 @@ ctx.strokeStyle = "lightgray";
 ctx.lineWidth = 2;
 */
 
-function drawBoard(ctx){
-  ctx.moveTo(0,0);
-  ctx.lineTo(502,0);
-  ctx.lineTo(502,502);
-  ctx.lineTo(0,502);
-  ctx.lineTo(0,0);
-  ctx.moveTo(101,0);
-  ctx.lineTo(101,502);
-  ctx.moveTo(201,0);
-  ctx.lineTo(201,502);
-  ctx.moveTo(301,0);
-  ctx.lineTo(301,502);
-  ctx.moveTo(401,0);
-  ctx.lineTo(401,502);
-  ctx.moveTo(0,101);
-  ctx.lineTo(502,101);
-  ctx.moveTo(0,201);
-  ctx.lineTo(502,201);
-  ctx.moveTo(0,301);
-  ctx.lineTo(502,301);
-  ctx.moveTo(0,401);
-  ctx.lineTo(502,401);
-  ctx.stroke();
-}
-
 drawBoard(ctxBoard);
 
 // make shapes
@@ -49,6 +24,8 @@ for (var i=0; i<5; i++) {
     boardFields.push( new Square( { x:1+i*100, y:1+j*100 }, 100 ) );
   }
 }
+
+drawPieces(ctxBoard);
 
 // listen for mousedown events
 $("#boardCanvas").mousedown(function (e) {
